7622bf7a4b34dbb1a33b065f83a175d7b47e114c
Quin F. <quin@cheesits456.dev>
2025-09-16 21:46:30 -0700

    Add comments

--- compilers/zip-output.sh
+++ compilers/zip-output.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Create zipped subdirectory if it doesn't already exist
 if [ ! -d "./dist/zipped" ]; then
 	mkdir -p "./dist/zipped"
 fi
 
+# Enter ./dist folder
 cd "./dist" || exit
 
+# Zip every subdirectory sequentially except zipped folder itself
 for i in ./*; do
 	if [ "$i" = "./zipped" ]; then
 		continue
@@ -15,4 +18,5 @@ for i in ./*; do
 	echo "Done!"
 done
 
+# Move new .zip files into zipped subdirectory
 mv ./*.zip ./zipped
