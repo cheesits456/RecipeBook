0ff350f9a2433b36b36298a50471ecc95bc90222
Quin F. <quin@cheesits456.dev>
2025-09-18 17:12:32 -0700

    Move fraction replacement to its own function

--- page/script.js
+++ page/script.js
@@ -286,26 +286,7 @@ function showRecipePage(recipePath) {
 				</center>
 			</div>
 			<div class="col-md-11">
-				<label for="ingredient-${recipe.ingredients.indexOf(ingredient)}" class="hover-pointer">${ingredient
-				.replace(/1\/2/g, "½")
-				.replace(/1\/3/g, "⅓")
-				.replace(/1\/4/g, "¼")
-				.replace(/1\/5/g, "⅕")
-				.replace(/1\/6/g, "⅙")
-				.replace(/1\/7/g, "⅐")
-				.replace(/1\/8/g, "⅛")
-				.replace(/1\/9/g, "⅑")
-				.replace(/1\/10/g, "⅒")
-				.replace(/2\/3/g, "⅔")
-				.replace(/2\/5/g, "⅖")
-				.replace(/3\/5/g, "⅗")
-				.replace(/3\/8/g, "⅜")
-				.replace(/4\/5/g, "⅘")
-				.replace(/3\/4/g, "¾")
-				.replace(/5\/6/g, "⅚")
-				.replace(/5\/8/g, "⅝")
-				.replace(/7\/8/g, "⅞")
-			}</label>
+				<label for="ingredient-${recipe.ingredients.indexOf(ingredient)}" class="hover-pointer">${replaceFractions(ingredient)}</label>
 			</div>
 		`
 	};
@@ -385,4 +366,26 @@ function createNewRecipe() {
 function editRecipe(title) {
 	const data = JSON.parse(fs.readFileSync(path.join(recipeDirectory, `${title}.recipe`)));
 	showCreate(data);
+}
+
+function replaceFractions(text) {
+	return text
+		.replace(/1\/2/g, "½")
+		.replace(/1\/3/g, "⅓")
+		.replace(/1\/4/g, "¼")
+		.replace(/1\/5/g, "⅕")
+		.replace(/1\/6/g, "⅙")
+		.replace(/1\/7/g, "⅐")
+		.replace(/1\/8/g, "⅛")
+		.replace(/1\/9/g, "⅑")
+		.replace(/1\/10/g, "⅒")
+		.replace(/2\/3/g, "⅔")
+		.replace(/2\/5/g, "⅖")
+		.replace(/3\/5/g, "⅗")
+		.replace(/3\/8/g, "⅜")
+		.replace(/4\/5/g, "⅘")
+		.replace(/3\/4/g, "¾")
+		.replace(/5\/6/g, "⅚")
+		.replace(/5\/8/g, "⅝")
+		.replace(/7\/8/g, "⅞");
 }
