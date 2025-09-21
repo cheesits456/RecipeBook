495fc6b98c9ee3ec1e4c8c59b2128c95a080fa56
Quin F. <quin@cheesits456.dev>
2025-09-20 21:34:31 -0700

    Fix wrong coloration of a bunch of elements in darkmode

--- page/script.js
+++ page/script.js
@@ -9,8 +9,15 @@ process.chdir(__dirname);
 const recipeDirectory = path.join(os.homedir(), "Documents", "Recipes");
 if (!fs.existsSync(recipeDirectory)) fs.mkdir(recipeDirectory, null, () => { });
 
+let config = {};
+if (fs.existsSync(path.join(recipeDirectory, "RecipeBook.config"))) {
+	try {
+		config = JSON.parse(fs.readFileSync(path.join(recipeDirectory, "RecipeBook.config")))
+	} catch { };
+}
+
 // Set variable for whether darkmode is enabled
-let darkmode = false;
+let darkmode = config.darkmode || false;
 let darkmodeCounter = 0;
 
 // Set the copyright year and version number
@@ -18,6 +25,7 @@ document.getElementById("copyright-version").innerHTML = JSON.parse(fs.readFileS
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
 updateSidebar();
+changeColor(config.color || "blue");
 
 // Display the landing page
 showMain();
@@ -202,6 +210,7 @@ function showRecipes(mealType) {
 
 	const fileNames = fs.readdirSync(recipeDirectory);
 	for (const fileName of fileNames) {
+		if (!fileName.endsWith(".recipe")) continue;
 		let recipe = {};
 		try {
 			recipe = JSON.parse(fs.readFileSync(path.join(recipeDirectory, fileName)));
@@ -440,8 +449,17 @@ function renameRecipe() {
 
 function changeColor(color) {
 	let root = document.querySelector(":root");
+	let config = {};
+	if (fs.existsSync(path.join(recipeDirectory, "RecipeBook.config"))) {
+		try {
+			config = JSON.parse(fs.readFileSync(path.join(recipeDirectory, "RecipeBook.config")))
+		} catch { };
+	};
+	config.color = color;
+	fs.writeFileSync(path.join(recipeDirectory, "RecipeBook.config"), JSON.stringify(config, null, "\t"));
 	if (color === "red") {
 		root.style.setProperty("--background", darkmode ? "#3f3b3b" : "#fff5f5");
+		root.style.setProperty("--text", darkmode ? "#eee" : "black");
 		root.style.setProperty("--primary", "#d81515");
 		root.style.setProperty("--primary-transparent", "#d8151522");
 		root.style.setProperty("--secondary", "#961111");
@@ -450,6 +468,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary-transparent", "#460c0c22");
 	} else if (color === "orange") {
 		root.style.setProperty("--background", darkmode ? "#3f3e3b" : "#fffaf5");
+		root.style.setProperty("--text", darkmode ? "#eee" : "black");
 		root.style.setProperty("--primary", "#e77e05");
 		root.style.setProperty("--primary-transparent", "#e77e0522");
 		root.style.setProperty("--secondary", "#aa5b00");
@@ -458,6 +477,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary-transparent", "#381e0022");
 	} else if (color === "green") {
 		root.style.setProperty("--background", darkmode ? "#3b3f3b" : "#f0fff1");
+		root.style.setProperty("--text", darkmode ? "#eee" : "black");
 		root.style.setProperty("--primary", "#45d122");
 		root.style.setProperty("--primary-transparent", "#45d12222");
 		root.style.setProperty("--secondary", "#2b8315");
@@ -466,6 +486,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary-transparent", "#0a240322");
 	} else if (color === "blue") {
 		root.style.setProperty("--background", darkmode ? "#3b3d3f" : "#f0f4ff");
+		root.style.setProperty("--text", darkmode ? "#eee" : "black");
 		root.style.setProperty("--primary", "#4f52ba");
 		root.style.setProperty("--primary-transparent", "#4f52ba22");
 		root.style.setProperty("--secondary", "#353577");
@@ -474,6 +495,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary-transparent", "#161a2d22");
 	} else if (color === "purple") {
 		root.style.setProperty("--background", darkmode ? "#3e3b3f" : "#f9f0ff");
+		root.style.setProperty("--text", darkmode ? "#eee" : "black");
 		root.style.setProperty("--primary", "#9122d1");
 		root.style.setProperty("--primary-transparent", "#8b22d122");
 		root.style.setProperty("--secondary", "#4e1172");
@@ -488,8 +510,16 @@ function toggleDarkmode() {
 	darkmodeCounter++;
 	if (darkmodeCounter === 2) darkmodeCounter = 0;
 	if (darkmodeCounter === 1) {
+		let config = {};
+		if (fs.existsSync(path.join(recipeDirectory, "RecipeBook.config"))) {
+			try {
+				config = JSON.parse(fs.readFileSync(path.join(recipeDirectory, "RecipeBook.config")))
+			} catch { };
+		};
 		if (!darkmode) darkmode = true;
 		else darkmode = false;
+		config.darkmode = darkmode;
+		fs.writeFileSync(path.join(recipeDirectory, "RecipeBook.config"), JSON.stringify(config, null, "\t"));
 		let root = document.querySelector(":root");
 		let color = window.getComputedStyle(document.getElementById("body")).backgroundColor;
 		switch (color) {
@@ -499,7 +529,7 @@ function toggleDarkmode() {
 				break;
 			case "rgb(255, 245, 245)":
 				root.style.setProperty("--background", "#3f3b3b");
-				root.style.setProperty("--text", "#ccc");
+				root.style.setProperty("--text", "#eee");
 				break;
 			case "rgb(63, 62, 59)":
 				root.style.setProperty("--background", "#fffaf5");
@@ -507,7 +537,7 @@ function toggleDarkmode() {
 				break;
 			case "rgb(255, 250, 245)":
 				root.style.setProperty("--background", "#3f3e3b");
-				root.style.setProperty("--text", "#ccc");
+				root.style.setProperty("--text", "#eee");
 				break;
 			case "rgb(59, 63, 59)":
 				root.style.setProperty("--background", "#f0fff1");
@@ -515,7 +545,7 @@ function toggleDarkmode() {
 				break;
 			case "rgb(240, 255, 241)":
 				root.style.setProperty("--background", "#3b3f3b");
-				root.style.setProperty("--text", "#ccc");
+				root.style.setProperty("--text", "#eee");
 				break;
 			case "rgb(59, 61, 63)":
 				root.style.setProperty("--background", "#f0f4ff");
@@ -523,7 +553,7 @@ function toggleDarkmode() {
 				break;
 			case "rgb(240, 244, 255)":
 				root.style.setProperty("--background", "#3b3d3f");
-				root.style.setProperty("--text", "#ccc");
+				root.style.setProperty("--text", "#eee");
 				break;
 			case "rgb(62, 59, 63)":
 				root.style.setProperty("--background", "#f9f0ff");
@@ -531,7 +561,7 @@ function toggleDarkmode() {
 				break;
 			case "rgb(249, 240, 255)":
 				root.style.setProperty("--background", "#3e3b3f");
-				root.style.setProperty("--text", "#ccc");
+				root.style.setProperty("--text", "#eee");
 				break;
 
 		}
