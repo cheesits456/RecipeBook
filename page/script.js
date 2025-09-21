2642b032ecf23e883fe01698b322f174dd19add9
Quin F. <quin@cheesits456.dev>
2025-09-20 18:23:57 -0700

    Set up toggleable darkmode
    todo: fix colors of a bunch of things when darkmode is enabled

--- page/script.js
+++ page/script.js
@@ -9,6 +9,10 @@ process.chdir(__dirname);
 const recipeDirectory = path.join(os.homedir(), "Documents", "Recipes");
 if (!fs.existsSync(recipeDirectory)) fs.mkdir(recipeDirectory, null, () => { });
 
+// Set variable for whether darkmode is enabled
+let darkmode = false;
+let darkmodeCounter = 0;
+
 // Set the copyright year and version number
 document.getElementById("copyright-version").innerHTML = JSON.parse(fs.readFileSync(path.join("..", "package.json").replace(/\\/g, "\\\\"))).version;
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
@@ -69,7 +73,11 @@ function showMain() {
 			<span class="orange-button hover-pointer" data-toggle="tooltip" data-placement="top" title="Change to Orange" onclick="changeColor('orange')">&nbsp; &nbsp; &nbsp; </span> &nbsp; &nbsp;
 			<span class="green-button hover-pointer" data-toggle="tooltip" data-placement="top" title="Change to Green" onclick="changeColor('green')">&nbsp; &nbsp; &nbsp; </span> &nbsp; &nbsp;
 			<span class="blue-button hover-pointer" data-toggle="tooltip" data-placement="top" title="Change to Blue" onclick="changeColor('blue')">&nbsp; &nbsp; &nbsp; </span> &nbsp; &nbsp;
-			<span class="purple-button hover-pointer" data-toggle="tooltip" data-placement="top" title="Change to Purple" onclick="changeColor('purple')">&nbsp; &nbsp; &nbsp; </span>&nbsp;
+			<span class="purple-button hover-pointer" data-toggle="tooltip" data-placement="top" title="Change to Purple" onclick="changeColor('purple')">&nbsp; &nbsp; &nbsp; </span> &nbsp; &nbsp;
+			<label class="switch" onclick="toggleDarkmode()">
+				<input id="darkmode-slider" type="checkbox" ${darkmode ? "checked" : ""}>
+				<span class="slider round"></span>
+			</label>
 		</div>
 		<div class="landing-body">
 			<div class="size-13">Browse recipe categories on the left</div>
@@ -433,7 +441,7 @@ function renameRecipe() {
 function changeColor(color) {
 	let root = document.querySelector(":root");
 	if (color === "red") {
-		root.style.setProperty("--background", "#fff5f5");
+		root.style.setProperty("--background", darkmode ? "#3f3b3b" : "#fff5f5");
 		root.style.setProperty("--primary", "#d81515");
 		root.style.setProperty("--primary-transparent", "#d8151522");
 		root.style.setProperty("--secondary", "#961111");
@@ -441,7 +449,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary", "#460c0c");
 		root.style.setProperty("--quartinary-transparent", "#460c0c22");
 	} else if (color === "orange") {
-		root.style.setProperty("--background", "#fffaf5");
+		root.style.setProperty("--background", darkmode ? "#3f3e3b" : "#fffaf5");
 		root.style.setProperty("--primary", "#e77e05");
 		root.style.setProperty("--primary-transparent", "#e77e0522");
 		root.style.setProperty("--secondary", "#aa5b00");
@@ -449,7 +457,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary", "#381e00");
 		root.style.setProperty("--quartinary-transparent", "#381e0022");
 	} else if (color === "green") {
-		root.style.setProperty("--background", "#f0fff1");
+		root.style.setProperty("--background", darkmode ? "#3b3f3b" : "#f0fff1");
 		root.style.setProperty("--primary", "#45d122");
 		root.style.setProperty("--primary-transparent", "#45d12222");
 		root.style.setProperty("--secondary", "#2b8315");
@@ -457,7 +465,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary", "#0a2403");
 		root.style.setProperty("--quartinary-transparent", "#0a240322");
 	} else if (color === "blue") {
-		root.style.setProperty("--background", "#f0f4ff");
+		root.style.setProperty("--background", darkmode ? "#3b3d3f" : "#f0f4ff");
 		root.style.setProperty("--primary", "#4f52ba");
 		root.style.setProperty("--primary-transparent", "#4f52ba22");
 		root.style.setProperty("--secondary", "#353577");
@@ -465,7 +473,7 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary", "#161a2d");
 		root.style.setProperty("--quartinary-transparent", "#161a2d22");
 	} else if (color === "purple") {
-		root.style.setProperty("--background", "#f9f0ff");
+		root.style.setProperty("--background", darkmode ? "#3e3b3f" : "#f9f0ff");
 		root.style.setProperty("--primary", "#9122d1");
 		root.style.setProperty("--primary-transparent", "#8b22d122");
 		root.style.setProperty("--secondary", "#4e1172");
@@ -473,5 +481,59 @@ function changeColor(color) {
 		root.style.setProperty("--quartinary", "#1f062e");
 		root.style.setProperty("--quartinary-transparent", "#1f062e22");
 
+	}
+}
+
+function toggleDarkmode() {
+	darkmodeCounter++;
+	if (darkmodeCounter === 2) darkmodeCounter = 0;
+	if (darkmodeCounter === 1) {
+		if (!darkmode) darkmode = true;
+		else darkmode = false;
+		let root = document.querySelector(":root");
+		let color = window.getComputedStyle(document.getElementById("body")).backgroundColor;
+		switch (color) {
+			case "rgb(63, 59, 59)":
+				root.style.setProperty("--background", "#fff5f5");
+				root.style.setProperty("--text", "black");
+				break;
+			case "rgb(255, 245, 245)":
+				root.style.setProperty("--background", "#3f3b3b");
+				root.style.setProperty("--text", "#ccc");
+				break;
+			case "rgb(63, 62, 59)":
+				root.style.setProperty("--background", "#fffaf5");
+				root.style.setProperty("--text", "black");
+				break;
+			case "rgb(255, 250, 245)":
+				root.style.setProperty("--background", "#3f3e3b");
+				root.style.setProperty("--text", "#ccc");
+				break;
+			case "rgb(59, 63, 59)":
+				root.style.setProperty("--background", "#f0fff1");
+				root.style.setProperty("--text", "black");
+				break;
+			case "rgb(240, 255, 241)":
+				root.style.setProperty("--background", "#3b3f3b");
+				root.style.setProperty("--text", "#ccc");
+				break;
+			case "rgb(59, 61, 63)":
+				root.style.setProperty("--background", "#f0f4ff");
+				root.style.setProperty("--text", "black");
+				break;
+			case "rgb(240, 244, 255)":
+				root.style.setProperty("--background", "#3b3d3f");
+				root.style.setProperty("--text", "#ccc");
+				break;
+			case "rgb(62, 59, 63)":
+				root.style.setProperty("--background", "#f9f0ff");
+				root.style.setProperty("--text", "black");
+				break;
+			case "rgb(249, 240, 255)":
+				root.style.setProperty("--background", "#3e3b3f");
+				root.style.setProperty("--text", "#ccc");
+				break;
+
+		}
 	}
 }
